513eecd5506ac13aeeabba3037901cddf6c3379a
sunbo <sunbo_hi@qq.com>
2016-11-08 17:49:59 +0800

    solve conflict

--- sisyphus.py
+++ sisyphus.py
@@ -6,9 +6,9 @@ import time
 
 
 def reset_link() -> None:
-    child = pexpect.spawn('ssh -CfnNT -R 2333:localhost:22 115.28.182.224 -p 2200 -l root')
-    child.expect("root@115.28.182.224's password:")
-    child.sendline("Sunbo220502")
+    child = pexpect.spawn('ssh -CfnNT -R 2333:localhost:22 100.100.100.100 -p 22 -l root')
+    child.expect("root@100.100.100.100's password:")
+    child.sendline("haha")
 
 
 def sisyphus() -> None:
@@ -21,7 +21,7 @@ def sisyphus() -> None:
         s = pxssh.pxssh()
 
         try:
-            s.login("115.28.182.224", "root", "Sunbo220502", port="2200")
+            s.login("100.100.100.100", "root", "haha", port="22")
         except pxssh.ExceptionPxssh as e:
             print("pxssh failed on login.")
             print(e)
@@ -29,7 +29,7 @@ def sisyphus() -> None:
         try:
             s.sendline('ssh 127.0.0.1 -p 2333 -l pi')
             s.expect("pi@127.0.0.1's password:")
-            s.sendline('sunbo1')
+            s.sendline('haha')
         except pexpect.ExceptionPexpect as e:
             reset_link()
             print(e)
