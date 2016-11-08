5213055e524f8d8dc98f8445d1fa8d21dec1cf0c
sunbo <sunbo_hi@qq.com>
2016-11-08 16:01:42 +0800

    add fail-recover

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 from pexpect import pxssh
+import pexpect
+
+
+def reset_link() -> None:
+    child = pexpect.spawn('ssh -CfnNT -R 2333:localhost:22 115.28.182.224 -p 2200 -l root')
+    child.expect("root@115.28.182.224's password:")
+    child.sendline("Sunbo220502")
 
 
 def main() -> None:
     try:
         s = pxssh.pxssh()
-        s.login("115.28.182.224", "root", "Sunbo220502", port="2200")
+        s.login("115.28.182.224", "root", "Sunbo220502", port="2333")
         s.sendline('who')
         s.prompt()
         print(s.before)
